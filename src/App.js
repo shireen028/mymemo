3429ad8df9fc490d23dd4f51a979d104d09f90b1
shireen028 <shireen@gleecus.com>
2022-11-16 12:04:46 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Experience from "./components/Experience";
-import Projects from "./components/Projects";
+//import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 class App extends Component {
